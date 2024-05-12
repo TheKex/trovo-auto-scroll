d94105e90f2414d61f719eb67e4e11de594529d5
TheKex <32483684+TheKex@users.noreply.github.com>
2024-05-12 20:25:33 +0300

    Create trovo-auto-scroll.js

---
+++ trovo-auto-scroll.js
@@ -0,0 +1,30 @@
+// ==UserScript==
+// @name         Trovo auto-scroll fix
+// @namespace    http://tampermonkey.net/
+// @version      2024-05-12
+// @description  try to take over the world!
+// @author       You
+// @match        https://trovo.live/chat/*
+// @match        https://trovo.live/s/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=trovo.live
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    function check_tip() {
+        let tip = document.querySelector(".at-tip");
+        if (tip !== null) {
+
+            tip.click();
+        }
+        tip = document.querySelector(".read-tip");
+        if (tip !== null) {
+            tip.click();
+        }
+    };
+
+    setInterval(check_tip, 20000);
+
+})();
